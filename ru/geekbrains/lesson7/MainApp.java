f35df27841b557661e16837a92cbd55ae6c8505a
Nikolambas <88347156+Nikolambas@users.noreply.github.com>
2021-08-24 22:28:07 +0300

    Update MainApp.java

--- ru/geekbrains/lesson7/MainApp.java
+++ ru/geekbrains/lesson7/MainApp.java
@@ -1,19 +1,1 @@
-package ru.geekbrains.lesson7;
 
-import java.util.Arrays;
-
-public class MainApp {
-    public static void main(String[] args) {
-        Cat [] cats = {
-                new Cat("Барсик"),
-                new Cat("Белка"),
-                new Cat("Стрелка")
-        };
-        Plate plate = new Plate(12);
-        for (Cat cat:cats) {
-            cat.eat(plate);
-            System.out.println(plate.getFood());
-            System.out.println(cat.isHungry());
-        }
-    }
-}
